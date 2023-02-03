5a3d62473230e9fdffab3efcc238ac41bc67d370
CYBER NETICS <cybernetics.me@outlook.com>
2023-02-03 14:49:27 +0530

    first commit

---
+++ resources/views/base.blade.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="">
+        <meta name="keywords" content="">
+		<title>Aurega Corporate Services</title>
+		<!-- Favicon -->
+        <link href="{{ public_path().'/assets/images/favicon.png' }}" rel="shortcut icon">
+		<!-- CSS -->
+		<link href="{{ public_path().'/assets/plugins/bootstrap/bootstrap.min.css' }}" rel="stylesheet">
+		<link href="{{ public_path().'/assets/plugins/owl-carousel/owl.carousel.min.css' }}" rel="stylesheet">
+		<link href="{{ public_path().'/assets/plugins/owl-carousel/owl.theme.default.min.css' }}" rel="stylesheet">
+		<link href="{{ public_path().'/assets/plugins/magnific-popup/magnific-popup.min.css' }}" rel="stylesheet">
+		<link href="{{ public_path().'/assets/plugins/sal/sal.min.css' }}" rel="stylesheet">
+		<link href="{{ public_path().'/assets/css/theme.css' }}" rel="stylesheet">
+		<!-- Fonts/Icons -->
+		<link href="{{ public_path().'/assets/plugins/bootstrap-icons/bootstrap-icons.css' }}" rel="stylesheet">
+		<link href="{{ public_path().'/assets/plugins/font-awesome/css/all.css' }}" rel="stylesheet">
+	</head>
+	<body data-preloader="1">
+		
+		<!-- Header -->
+		<div class="header right dark sticky-autohide">
+			<div class="container">
+				<!-- Logo -->
+				<div class="header-logo">
+					<h3><a href="#">mono</a></h3>
+					<!-- 
+					<img class="logo-dark" src="../assets/images/your-logo-dark.png" alt="">
+					<img class="logo-light" src="../assets/images/your-logo-light.png" alt=""> 
+					-->
+				</div>
+				<!-- Menu -->
+				<div class="header-menu">
+					<ul class="nav">
+						<li class="nav-item">
+							<a class="nav-link" href="#">Link Only</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Dropdown</a>
+							<ul class="nav-dropdown">
+								<li class="nav-dropdown-item"><a class="nav-dropdown-link" href="#">Dropdown Item</a></li>
+								<li class="nav-dropdown-item"><a class="nav-dropdown-link" href="#">Dropdown Item</a></li>
+								<li class="nav-dropdown-item"><a class="nav-dropdown-link" href="#">Dropdown Item</a></li>
+								<li class="nav-dropdown-item"><a class="nav-dropdown-link" href="#">Dropdown Item</a></li>
+							</ul>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Subdropdown</a>
+							<ul class="nav-dropdown">
+								<li class="nav-dropdown-item">
+									<a class="nav-dropdown-link" href="#">Dropdown Item</a>
+									<ul class="nav-subdropdown">
+										<li class="nav-subdropdown-item"><a class="nav-subdropdown-link" href="#">Subdropdown Item</a></li>
+										<li class="nav-subdropdown-item"><a class="nav-subdropdown-link" href="#">Subdropdown Item</a></li>
+										<li class="nav-subdropdown-item"><a class="nav-subdropdown-link" href="#">Subdropdown Item</a></li>
+										<li class="nav-subdropdown-item"><a class="nav-subdropdown-link" href="#">Subdropdown Item</a></li>
+									</ul>
+								</li>
+								<li class="nav-dropdown-item">
+									<a class="nav-dropdown-link" href="#">Dropdown Item</a>
+									<ul class="nav-subdropdown">
+										<li class="nav-subdropdown-item"><a class="nav-subdropdown-link" href="#">Subdropdown Item</a></li>
+										<li class="nav-subdropdown-item"><a class="nav-subdropdown-link" href="#">Subdropdown Item</a></li>
+										<li class="nav-subdropdown-item"><a class="nav-subdropdown-link" href="#">Subdropdown Item</a></li>
+										<li class="nav-subdropdown-item"><a class="nav-subdropdown-link" href="#">Subdropdown Item</a></li>
+									</ul>
+								</li>
+							</ul>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Subdropdown LG</a>
+							<ul class="nav-dropdown">
+								<li class="nav-dropdown-item">
+									<a class="nav-dropdown-link" href="#">Dropdown Item</a>
+									<div class="nav-subdropdown nav-subdropdown-lg">
+										<div class="row g-2 g-lg-3">
+											<div class="col-12 col-lg-6">
+												<ul>
+													<li class="nav-subdropdown-item"><a class="nav-subdropdown-link" href="#">Subdropdown Item</a></li>
+													<li class="nav-subdropdown-item"><a class="nav-subdropdown-link" href="#">Subdropdown Item</a></li>
+													<li class="nav-subdropdown-item"><a class="nav-subdropdown-link" href="#">Subdropdown Item</a></li>
+													<li class="nav-subdropdown-item"><a class="nav-subdropdown-link" href="#">Subdropdown Item</a></li>
+												</ul>
+											</div>
+											<div class="col-12 col-lg-6">
+												<ul>
+													<li class="nav-subdropdown-item"><a class="nav-subdropdown-link" href="#">Subdropdown Item</a></li>
+													<li class="nav-subdropdown-item"><a class="nav-subdropdown-link" href="#">Subdropdown Item</a></li>
+													<li class="nav-subdropdown-item"><a class="nav-subdropdown-link" href="#">Subdropdown Item</a></li>
+													<li class="nav-subdropdown-item"><a class="nav-subdropdown-link" href="#">Subdropdown Item</a></li>
+												</ul>
+											</div>
+										</div><!-- end row -->
+									</div>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+				<!-- Menu Extra -->
+				<div class="header-menu-extra">
+					<ul class="list-inline">
+						<li><a href="#"><i class="bi bi-facebook"></i></a></li>
+						<li><a href="#"><i class="bi bi-twitter"></i></a></li>
+						<li><a href="#"><i class="bi bi-linkedin"></i></a></li>
+					</ul>
+				</div>
+				<!-- Menu Toggle -->
+				<button class="header-toggle">
+					<span></span>
+				</button>
+			</div><!-- end container -->
+		</div>
+		<!-- end Header -->
+
+		@yield("content")
+
+		<footer>
+			<div class="section-sm bg-dark">
+				<div class="container">
+					<div class="row g-4">
+						<div class="col-6 col-sm-6 col-lg-3">
+							<h3 class="uppercase letter-spacing-1">mono</h3>
+						</div>
+						<div class="col-6 col-sm-6 col-lg-3">
+							<h6 class="font-small fw-medium uppercase">Useful Links</h6>
+							<ul class="list-dash animate-links">
+								<li><a href="#">About us</a></li>
+								<li><a href="#">Team</a></li>
+								<li><a href="#">Prices</a></li>
+								<li><a href="#">Contact</a></li>
+							</ul>
+						</div>
+						<div class="col-6 col-sm-6 col-lg-3">
+							<h6 class="font-small fw-medium uppercase">Additional Links</h6>
+							<ul class="list-dash animate-links">
+								<li><a href="#">Services</a></li>
+								<li><a href="#">Process</a></li>
+								<li><a href="#">FAQ</a></li>
+								<li><a href="#">Careers</a></li>
+							</ul>
+						</div>
+						<div class="col-6 col-sm-6 col-lg-3">
+							<h6 class="font-small fw-medium uppercase">Contact Info</h6>
+							<ul class="list-unstyled">
+								<li>121 King St, Melbourne VIC 3000</li>
+								<li>contact@example.com</li>
+								<li>+(123) 456 789 01</li>
+							</ul>
+						</div>
+					</div><!-- end row -->
+				</div><!-- end container -->
+			</div>
+			<div class="bg-black py-4">	
+				<div class="container">
+					<div class="row align-items-center g-2 g-lg-3">
+						<div class="col-12 col-md-6 text-center text-md-start">
+							<p>&copy; 2023 FlaTheme, All Rights Reserved.</p>
+						</div>
+						<div class="col-12 col-md-6 text-center text-md-end">
+							<ul class="list-inline-sm">
+								<li><a class="button-circle button-circle-sm button-circle-social-facebook" href="#"><i class="bi bi-facebook"></i></a></li>
+								<li><a class="button-circle button-circle-sm button-circle-social-twitter" href="#"><i class="bi bi-twitter"></i></a></li>
+								<li><a class="button-circle button-circle-sm button-circle-social-pinterest" href="#"><i class="bi bi-pinterest"></i></a></li>
+								<li><a class="button-circle button-circle-sm button-circle-social-instagram" href="#"><i class="bi bi-instagram"></i></a></li>
+							</ul>
+						</div>
+					</div><!-- end row -->
+				</div><!-- end container -->
+			</div>
+		</footer>
+
+		<!-- Scroll to top button -->
+		<div class="scrolltotop">
+			<a class="button-circle button-circle-md button-circle-dark" href="#"><i class="bi bi-arrow-up"></i></a>
+		</div>
+		<!-- end Scroll to top button -->
+
+		<!-- ***** JAVASCRIPTS ***** -->
+		<script src="{{ public_path().'/assets/plugins/jquery.min.js' }}"></script>
+		<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY"></script>
+		<script src="https://polyfill.io/v3/polyfill.min.js?features=IntersectionObserver"></script>
+		<script src="{{ public_path().'/assets/plugins/plugins.js' }}"></script>
+		<script src="{{ public_path().'/assets/js/functions.js' }}"></script>
+	</body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,244 @@
+@extends("base")
+@section("content")
+<!-- Hero section -->
+<div class="section-xl bg-image parallax" data-bg-src="../assets/images/background.jpg">
+    <div class="bg-dark-05">
+        <div class="container">
+            <div class="row g-4">
+                <div class="col-12 col-sm-10 col-md-8">
+                    <div class="bg-color-spring-red-07 p-5 border-radius">
+                        <h2 class="fw-light m-0">Great things in business are never done by one person. They're done by a team of people.</h2>
+                    </div>
+                </div>
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </div>
+</div>
+<!-- end Hero section -->
+<!-- Services section -->
+<div class="section">
+    <div class="container">
+        <div class="row icon-5xl g-4">
+            <!-- Service box 1 -->
+            <div class="col-12 col-md-4">
+                <i class="bi bi-chat-text text-spring-red"></i>
+                <h5 class="fw-medium mt-2">Business Consultent</h5>
+                <p>Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.</p>
+                <a class="button-text-3 mt-3" href="#">Learn More</a>
+            </div>
+            <!-- Service box 2 -->
+            <div class="col-12 col-md-4">
+                <i class="bi bi-gear text-spring-red"></i>
+                <h5 class="fw-medium mt-2">Factory Consultent</h5>
+                <p>Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.</p>
+                <a class="button-text-3 mt-3" href="#">Learn More</a>
+            </div>
+            <!-- Service box 3 -->
+            <div class="col-12 col-md-4">
+                <i class="bi bi-journal-text text-spring-red"></i>
+                <h5 class="fw-medium mt-2">Strategy &amp; Planning</h5>
+                <p>Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.</p>
+                <a class="button-text-3 mt-3" href="#">Learn More</a>
+            </div>
+        </div><!-- end row -->
+    </div><!-- end container -->
+</div>
+<!-- end Services section -->
+
+<!-- About section -->
+<div class="container">
+    <div class="row align-items-center g-5">
+        <div class="col-12 col-lg-6 col-xl-7">
+            <img class="border-radius" src="../assets/images/col-1.jpg" alt="">
+        </div>
+        <div class="col-12 col-lg-6 col-xl-5">
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+            <!-- Progress bars -->
+            <div class="mt-4">
+                <!-- Progress bar box 1 -->
+                <div class="progress-box progress-spring-red">
+                    <h6 class="fw-medium">Social Marketing</h6>
+                    <div class="animated-progress">
+                        <div data-progress="94"></div>
+                    </div>
+                </div>
+                <!-- Progress bar box 2 -->
+                <div class="progress-box progress-spring-red mt-4">
+                    <h6 class="fw-medium">Sales Training</h6>
+                    <div class="animated-progress">
+                        <div data-progress="88"></div>
+                    </div>
+                </div>
+                <!-- Progress bar box 3 -->
+                <div class="progress-box progress-spring-red mt-4">
+                    <h6 class="fw-medium">Copywriting</h6>
+                    <div class="animated-progress">
+                        <div data-progress="92"></div>
+                    </div>
+                </div>
+            </div>
+            <!-- end Progress bars -->
+        </div>
+    </div><!-- end row -->
+</div>
+<!-- end About section -->
+
+<!-- Clients section -->
+<div class="section">
+    <div class="container">
+        <div class="mb-5">
+            <div class="row text-center">
+                <div class="col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2">
+                    <h2 class="fw-light">Our Amazing Clients</h2>
+                </div>
+            </div>
+        </div>
+        <ul class="clients-grid column-5">
+            <li>
+                <a href="#"><img src="../assets/images/col-5.jpg" alt=""></a>
+            </li>
+            <li>
+                <a href="#"><img src="../assets/images/col-5.jpg" alt=""></a>
+            </li>
+            <li>
+                <a href="#"><img src="../assets/images/col-5.jpg" alt=""></a>
+            </li>
+            <li>
+                <a href="#"><img src="../assets/images/col-5.jpg" alt=""></a>
+            </li>
+            <li>
+                <a href="#"><img src="../assets/images/col-5.jpg" alt=""></a>
+            </li>
+            <li>
+                <a href="#"><img src="../assets/images/col-5.jpg" alt=""></a>
+            </li>
+            <li>
+                <a href="#"><img src="../assets/images/col-5.jpg" alt=""></a>
+            </li>
+            <li>
+                <a href="#"><img src="../assets/images/col-5.jpg" alt=""></a>
+            </li>
+            <li>
+                <a href="#"><img src="../assets/images/col-5.jpg" alt=""></a>
+            </li>
+            <li>
+                <a href="#"><img src="../assets/images/col-5.jpg" alt=""></a>
+            </li>
+        </ul>
+    </div><!-- end container -->
+</div>
+<!-- end Clients section -->
+
+<!-- Testimonial section -->
+<div class="section-xl bg-image parallax" data-bg-src="../assets/images/background.jpg">
+    <div class="bg-dark-05">
+        <div class="container">
+            <div class="owl-carousel" data-owl-dots="false" data-owl-nav="true" data-owl-margin="30" data-owl-xs="1" data-owl-sm="1" data-owl-md="1" data-owl-lg="2" data-owl-xl="2">
+                <!-- Testimonial Slider Item 1 -->
+                <div class="bg-color-spring-red-07 border-radius p-5">
+                    <div class="d-flex align-items-center mb-3">
+                        <div class="d-inline-block me-3">
+                            <img class="img-mask-avatar-sm" src="../assets/images/img-avatar-sm.jpg" alt="">
+                        </div>
+                        <div class="d-inline-block">
+                            <h5 class="fw-normal m-0 line-height-140">Emiley Haley</h5>
+                            <span class="font-small text-white-09">Manager - Mono</span>
+                        </div>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                </div>
+                <!-- Testimonial Slider Item 2 -->
+                <div class="bg-color-spring-red-07 border-radius p-5">
+                    <div class="d-flex align-items-center mb-3">
+                        <div class="d-inline-block me-3">
+                            <img class="img-mask-avatar-sm" src="../assets/images/img-avatar-sm.jpg" alt="">
+                        </div>
+                        <div class="d-inline-block">
+                            <h5 class="fw-normal m-0 line-height-140">Andrew Palmer</h5>
+                            <span class="font-small text-white-09">Developer - Mono</span>
+                        </div>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                </div>
+                <!-- Testimonial Slider Item 3 -->
+                <div class="bg-color-spring-red-07 border-radius p-5">
+                    <div class="d-flex align-items-center mb-3">
+                        <div class="d-inline-block me-3">
+                            <img class="img-mask-avatar-sm" src="../assets/images/img-avatar-sm.jpg" alt="">
+                        </div>
+                        <div class="d-inline-block">
+                            <h5 class="fw-normal m-0 line-height-140">Anna Mullen</h5>
+                            <span class="font-small text-white-09">Designer - Mono</span>
+                        </div>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                </div>
+                <!-- Testimonial Slider Item 4 -->
+                <div class="bg-color-spring-red-07 border-radius p-5">
+                    <div class="d-flex align-items-center mb-3">
+                        <div class="d-inline-block me-3">
+                            <img class="img-mask-avatar-sm" src="../assets/images/img-avatar-sm.jpg" alt="">
+                        </div>
+                        <div class="d-inline-block">
+                            <h5 class="fw-normal m-0 line-height-140">John Smith</h5>
+                            <span class="font-small text-white-09">Marketer - Mono</span>
+                        </div>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                </div>
+            </div><!-- end owl-carousel -->
+        </div><!-- end container -->
+    </div><!-- end bg-dark-* -->
+</div>
+<!-- end Testimonial section -->
+
+<!-- Contact section -->
+<div class="section">
+    <div class="container">
+        <div class="row g-4">
+            <!-- Contact Info -->
+            <div class="col-12 col-md-4">
+                <div class="mb-4">
+                    <h6 class="fw-medium m-0">Phone:</h6>
+                    <p>+(987) 654 321 01</p>
+                </div>
+                <div class="mb-4">
+                    <h6 class="fw-medium m-0">Email:</h6>
+                    <p>contact@email.com</p>
+                </div>
+                <div>
+                    <h6 class="fw-medium m-0">Address:</h6>
+                    <p>121 King St, Melbourne VIC 3000</p>
+                </div>
+            </div>
+            <!-- Contact Form -->
+            <div class="col-12 col-md-8">
+                <div class="contact-form text-end">
+                    <form method="post" id="contactform">
+                        <div class="row gx-3 gy-0">
+                            <div class="col-12 col-sm-6">
+                                <input type="text" id="name" name="name" placeholder="Name" required>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <input type="email" id="email" name="email" placeholder="E-Mail" required>
+                            </div>
+                        </div>
+                        <input type="text" id="subject" name="subject" placeholder="Subject" required>
+                        <textarea name="message" id="message" placeholder="Message"></textarea>
+                        <button class="button button-lg button-spring-red button-radius" type="submit">Send Message</button>
+                    </form>
+                    <!-- Submit result -->
+                    <div class="submit-result">
+                        <span id="success">Thank you! Your Message has been sent.</span>
+                        <span id="error">Something went wrong, Please try again!</span>
+                    </div>
+                </div>
+            </div>
+            <!-- end Contact Form -->
+        </div><!-- end row -->
+        <!-- Google Maps -->
+        <div id="map1" class="gmap gmap-md mt-5" data-latitude="51.513569" data-longitude="-0.123443"></div>
+    </div><!-- end container -->
+</div>
+<!-- end Contact section -->
+@endsection
