e783d22ab3546fe1e0eecd8ac2e2273748b303d0
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-02-14 20:33:59 +0530

    small updates

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -18,7 +18,7 @@ class EmailController extends Controller
         ]);
         $input = $request->all(); $to_email = Config::get('app.mail_to_address');
         Mail::send('email', ['user' => $input], function($message) use($to_email){
-            $message->to("vijoysniit@gmail.com");
+            $message->to($to_email);
             $message->subject('Aurega Corporate Service - New Message');
         });        
         return redirect()->back()->with('success','Thank you for contacting us. We will get back to you shortly');        
