a851c3d6c2d3a7828fc777f42f53d56fcc17abd7
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-02-20 22:09:35 +0530

    small updates

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,5 +1,4 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+@php echo "<xml version='1.0' encoding='UTF-8'>"; @endphp
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
         <loc>http://auregacs.com</loc>
