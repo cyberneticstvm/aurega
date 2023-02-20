a4f75dc1b54938198d1ecf3eec6f44047c16c01b
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-02-20 22:01:08 +0530

    small updates

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -2,7 +2,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
         <loc>http://auregacs.com</loc>
-        <lastmod>{{ Carbon::yesterday() }}</lastmod>
+        <lastmod>{{  Carbon\Carbon::yesterday() }}</lastmod>
         <changefreq>daily</changefreq>
         <priority>0.1</priority>
     </url>
