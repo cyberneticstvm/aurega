211ab4fa90b33f4335bb75c71dc797e5aa304122
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-02-22 13:52:48 +0530

    small updates

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -6,4 +6,70 @@
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
     </url>
+    <url>
+        <loc>http://auregacs.com/about</loc>
+        <lastmod>{{  Carbon\Carbon::yesterday() }}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.1</priority>
+    </url>
+    <url>
+        <loc>http://auregacs.com/services</loc>
+        <lastmod>{{  Carbon\Carbon::yesterday() }}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.1</priority>
+    </url>
+    <url>
+        <loc>http://auregacs.com/careers</loc>
+        <lastmod>{{  Carbon\Carbon::yesterday() }}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.1</priority>
+    </url>
+    <url>
+        <loc>http://auregacs.com/contact</loc>
+        <lastmod>{{  Carbon\Carbon::yesterday() }}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.1</priority>
+    </url>
+    <url>
+        <loc>http://auregacs.com/registration</loc>
+        <lastmod>{{  Carbon\Carbon::yesterday() }}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.1</priority>
+    </url>
+    <url>
+        <loc>http://auregacs.com/services/company-incorporation-and-trade-license</loc>
+        <lastmod>{{  Carbon\Carbon::yesterday() }}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.1</priority>
+    </url>
+    <url>
+        <loc>http://auregacs.com/services/golden-visa</loc>
+        <lastmod>{{  Carbon\Carbon::yesterday() }}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.1</priority>
+    </url>
+    <url>
+        <loc>http://auregacs.com/services/business-consulting</loc>
+        <lastmod>{{  Carbon\Carbon::yesterday() }}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.1</priority>
+    </url>
+    <url>
+        <loc>http://auregacs.com/services/accounting-and-book-keeping</loc>
+        <lastmod>{{  Carbon\Carbon::yesterday() }}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.1</priority>
+    </url>
+    <url>
+        <loc>http://auregacs.com/services/pro-service</loc>
+        <lastmod>{{  Carbon\Carbon::yesterday() }}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.1</priority>
+    </url>
+    <url>
+        <loc>http://auregacs.com/services/trademark-registration-and-intellectual-property</loc>
+        <lastmod>{{  Carbon\Carbon::yesterday() }}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.1</priority>
+    </url>
 </urlset>
