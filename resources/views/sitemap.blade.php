18e10226740d0107246b6fe1a63562bc3ecb7339
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-02-22 14:06:39 +0530

    small updates

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -2,73 +2,73 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
         <loc>http://auregacs.com</loc>
-        <lastmod>{{  Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
+        <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
     </url>
     <url>
         <loc>http://auregacs.com/about</loc>
-        <lastmod>{{  Carbon\Carbon::yesterday() }}</lastmod>
+        <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
     </url>
     <url>
         <loc>http://auregacs.com/services</loc>
-        <lastmod>{{  Carbon\Carbon::yesterday() }}</lastmod>
+        <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
     </url>
     <url>
         <loc>http://auregacs.com/careers</loc>
-        <lastmod>{{  Carbon\Carbon::yesterday() }}</lastmod>
+        <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
     </url>
     <url>
         <loc>http://auregacs.com/contact</loc>
-        <lastmod>{{  Carbon\Carbon::yesterday() }}</lastmod>
+        <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
     </url>
     <url>
         <loc>http://auregacs.com/registration</loc>
-        <lastmod>{{  Carbon\Carbon::yesterday() }}</lastmod>
+        <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
     </url>
     <url>
         <loc>http://auregacs.com/services/company-incorporation-and-trade-license</loc>
-        <lastmod>{{  Carbon\Carbon::yesterday() }}</lastmod>
+        <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
     </url>
     <url>
         <loc>http://auregacs.com/services/golden-visa</loc>
-        <lastmod>{{  Carbon\Carbon::yesterday() }}</lastmod>
+        <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
     </url>
     <url>
         <loc>http://auregacs.com/services/business-consulting</loc>
-        <lastmod>{{  Carbon\Carbon::yesterday() }}</lastmod>
+        <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
     </url>
     <url>
         <loc>http://auregacs.com/services/accounting-and-book-keeping</loc>
-        <lastmod>{{  Carbon\Carbon::yesterday() }}</lastmod>
+        <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
     </url>
     <url>
         <loc>http://auregacs.com/services/pro-service</loc>
-        <lastmod>{{  Carbon\Carbon::yesterday() }}</lastmod>
+        <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
     </url>
     <url>
         <loc>http://auregacs.com/services/trademark-registration-and-intellectual-property</loc>
-        <lastmod>{{  Carbon\Carbon::yesterday() }}</lastmod>
+        <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
     </url>
