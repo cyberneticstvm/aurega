1e4da3154d524908c30ebb84ebdc68018c0e51d8
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-02-22 14:01:02 +0530

    small updates

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -2,7 +2,7 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
         <loc>http://auregacs.com</loc>
-        <lastmod>{{  Carbon\Carbon::timezone(Config::get('app.timezone'))->toDateTimeString() }}</lastmod>
+        <lastmod>{{  Carbon\Carbon::yesterday() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
     </url>
