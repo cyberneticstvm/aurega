0387e263073a207c79a0a1d2c32d56af0363cdc3
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-02-24 17:19:57 +0530

    small updates

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,73 +1,73 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
-        <loc>http://auregacs.com</loc>
+        <loc>https://auregacs.com</loc>
         <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
     </url>
     <url>
-        <loc>http://auregacs.com/about</loc>
+        <loc>https://auregacs.com/about</loc>
         <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
     </url>
     <url>
-        <loc>http://auregacs.com/services</loc>
+        <loc>https://auregacs.com/services</loc>
         <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
     </url>
     <url>
-        <loc>http://auregacs.com/careers</loc>
+        <loc>https://auregacs.com/careers</loc>
         <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
     </url>
     <url>
-        <loc>http://auregacs.com/contact</loc>
+        <loc>https://auregacs.com/contact</loc>
         <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
     </url>
     <url>
-        <loc>http://auregacs.com/registration</loc>
+        <loc>https://auregacs.com/registration</loc>
         <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
     </url>
     <url>
-        <loc>http://auregacs.com/services/company-incorporation-and-trade-license</loc>
+        <loc>https://auregacs.com/services/company-incorporation-and-trade-license</loc>
         <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
     </url>
     <url>
-        <loc>http://auregacs.com/services/golden-visa</loc>
+        <loc>https://auregacs.com/services/golden-visa</loc>
         <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
     </url>
     <url>
-        <loc>http://auregacs.com/services/business-consulting</loc>
+        <loc>https://auregacs.com/services/business-consulting</loc>
         <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
     </url>
     <url>
-        <loc>http://auregacs.com/services/accounting-and-book-keeping</loc>
+        <loc>https://auregacs.com/services/accounting-and-book-keeping</loc>
         <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
     </url>
     <url>
-        <loc>http://auregacs.com/services/pro-service</loc>
+        <loc>https://auregacs.com/services/pro-service</loc>
         <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
     </url>
     <url>
-        <loc>http://auregacs.com/services/trademark-registration-and-intellectual-property</loc>
+        <loc>https://auregacs.com/services/trademark-registration-and-intellectual-property</loc>
         <lastmod>{{ Carbon\Carbon::yesterday()->tz('UTC')->toAtomString() }}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>0.1</priority>
