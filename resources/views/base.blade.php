f262b2a111998ccc937c251b791f1e9bb97f361e
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-02-25 17:24:35 +0530

    small updates

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -139,14 +139,14 @@
 					</div><!-- end row -->
 				</div><!-- end container -->
 			</div>
-			<script  type="text/javascript">  var config = {    phone :" 971506939558",    call :"Let\'s Talk",    position :"ww-right",    size : "ww-normal",    text : "Hi, I\'m interested in your service",    type: "ww-standard",    brand: "Aurega Corporate Services",    subtitle: "",    welcome: "Hi, how can we help you today?"  };  var proto = document.location.protocol, host = "cloudfront.net", url = proto + "//d3kzab8jj16n2f." + host;    var s = document.createElement("script"); s.type = "text/javascript"; s.async = true; s.src = url + "/v2/main.js";    s.onload = function () { tmWidgetInit(config) };    var x = document.getElementsByTagName("script")[0]; x.parentNode.insertBefore(s, x);</script>
 		</footer>
 
-		<!-- Scroll to top button
+		<div class="wachat"><a href="https://web.whatsapp.com/send?phone=+971528686158&amp;text=Hello! I am interested in your service" target="_blank" class="for_desktop"><img src="{{ public_path().'/landing/assets/img/whatsapp.png' }}"></a></div>
+		<!-- Scroll to top button-->
 		<div class="scrolltotop">
 			<a class="button-circle button-circle-md button-circle-dark" href="#"><i class="bi bi-arrow-up"></i></a>
 		</div>
-		end Scroll to top button -->
+		<!--end Scroll to top button -->
 
 		<!-- ***** JAVASCRIPTS ***** -->
 		<script src="{{ public_path().'/assets/plugins/jquery.min.js' }}"></script>
