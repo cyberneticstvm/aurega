6691d27d9dd2380d8d08b00b6854275806ab9f9a
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-03-01 21:18:49 +0530

    small updates

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -92,7 +92,7 @@
 				<div class="container">
 					<div class="row g-4">
 						<div class="col-6 col-sm-6 col-lg-3">
-							<img class="logo-footer" src="{{ public_path().'/assets/images/aurega/logo.jpeg' }}" alt="Aurega Corporate Services">
+							<a href="/"><img class="logo-footer" src="{{ public_path().'/assets/images/aurega/logo.jpeg' }}" alt="Aurega Corporate Services"></a>
 						</div>
 						<div class="col-6 col-sm-6 col-lg-3">
 							<h6 class="font-small fw-medium uppercase">Useful Links</h6>
@@ -141,11 +141,11 @@
 			</div>
 		</footer>
 		<script  type="text/javascript">  var config = {    phone :" 971506939558",    call :"Let\'s Talk",    position :"ww-right",    size : "ww-normal",    text : "Hi, I\'m interested in your service",    type: "ww-standard",    brand: "Aurega Corporate Services",    subtitle: "",    welcome: "Hi, how can we help you today?"  };  var proto = document.location.protocol, host = "cloudfront.net", url = proto + "//d3kzab8jj16n2f." + host;    var s = document.createElement("script"); s.type = "text/javascript"; s.async = true; s.src = url + "/v2/main.js";    s.onload = function () { tmWidgetInit(config) };    var x = document.getElementsByTagName("script")[0]; x.parentNode.insertBefore(s, x);</script>
-		<!-- Scroll to top button
+		<!-- Scroll to top button-->
 		<div class="scrolltotop">
 			<a class="button-circle button-circle-md button-circle-dark" href="#"><i class="bi bi-arrow-up"></i></a>
 		</div>
-		end Scroll to top button -->
+		<!--end Scroll to top button -->
 
 		<!-- ***** JAVASCRIPTS ***** -->
 		<script src="{{ public_path().'/assets/plugins/jquery.min.js' }}"></script>
