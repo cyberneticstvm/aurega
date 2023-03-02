14d51f5fbd9a9771e409dd8973d902222e15026c
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-03-02 18:26:17 +0530

    small updates

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -1,17 +1,30 @@
-@extends("base")
-@section("content")
-<div class="section">
-    <div class="container">
-        <div class="mb-5">
-            <div class="row text-center">
-                <div class="col-12">
-                    <h4 class="fw-light text-success">Thank you for contacting us. Our executive will reach out you shortly!</h4>
-                </div>
-            </div>
-        </div>
-        <div class="mt-5 text-center">
-            <a class="button button-lg button-rounded button-turquiose" href="/">Home</a>
-        </div>
-    </div><!-- end container -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11096916840"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'AW-11096916840');
+    </script>
+
+  <title>Aurega Corporate Services - Thank You</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+    <h5 class="text-success">Thank you for contacting us. Our executive will reach out you shortly!</h5>
+    <div class="mt-5 text-center">
+        <a class="button button-lg button-rounded button-turquiose" href="/registration">Home</a>
+    </div>
 </div>
-@endsection
+
+</body>
+</html>
